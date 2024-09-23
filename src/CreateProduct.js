206a68c88c8f4a9e696412c5cd293f0603d78cc5
Erick <ericktostesro@gmail.com>
2024-09-23 01:59:16 -0300

    modified:   src/CreateProduct.js

--- src/CreateProduct.js
+++ src/CreateProduct.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css'; // Importando o estilo do editor
+import 'react-quill/dist/quill.snow.css';
 import './CreateProduct.css';
 
 const CreateProduct = () => {
